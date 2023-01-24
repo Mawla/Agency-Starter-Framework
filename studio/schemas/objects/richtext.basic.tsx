d576e4b57386774f03dbd1fb31d0157210fc18f9
Arjen <aratramba@gmail.com>
2023-01-24 07:42:39 +0100

    fix links opening in modal

--- studio/schemas/objects/richtext.basic.tsx
+++ studio/schemas/objects/richtext.basic.tsx
@@ -1,43 +1,42 @@
-import React from 'react'
-
-import {CustomRichTextEditor} from '../../components/CustomRichTextEditor'
+import { CustomRichTextEditor } from "../../components/CustomRichTextEditor";
+import React from "react";
 
 export default {
-  name: 'richtext.basic',
-  title: 'Rich Text',
-  type: 'array',
+  name: "richtext.basic",
+  title: "Rich Text",
+  type: "array",
   components: {
     input: CustomRichTextEditor,
   },
   of: [
     {
-      type: 'block',
-      title: 'Rich text',
+      type: "block",
+      title: "Rich text",
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
+        { title: "Normal", value: "normal" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
       ],
       lists: [
-        {title: 'Bullet', value: 'bullet'},
-        {title: 'Number', value: 'number'},
+        { title: "Bullet", value: "bullet" },
+        { title: "Number", value: "number" },
       ],
       marks: {
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
         ],
         annotations: [
           {
-            name: 'link',
-            title: 'Link',
-            type: 'link',
+            name: "link",
+            title: "Link",
+            type: "link",
             options: {
-              editModal: 'dialog',
+              modal: { type: "dialog" },
             },
           },
         ],
       },
     },
   ],
-}
+};
