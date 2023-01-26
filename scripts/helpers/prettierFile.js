b19726dacdf181733b1dbbad1fcbb108e332713d
Arjen <aratramba@gmail.com>
2023-01-26 12:09:22 +0100

    Update prettierFile.js

--- scripts/helpers/prettierFile.js
+++ scripts/helpers/prettierFile.js
@@ -1,15 +1,15 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 module.exports.prettierFile = (filePath) => {
   exec(
-    `${__dirname}/../../node_modules/prettier/bin-prettier.js --write ${filePath}`,
+    `${__dirname}/../../node_modules/prettier/bin-prettier.js --write ${filePath} --trailing-comma all`,
     (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
         return;
       }
       if (stderr) {
-        if (stderr.indexOf('[warn] ') > -1) return;
+        if (stderr.indexOf("[warn] ") > -1) return;
         console.log(stderr);
         return;
       }
