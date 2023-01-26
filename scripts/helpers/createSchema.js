728205aa5066ddc5bb70eedbaa23ac24c16d7b9d
Arjen <aratramba@gmail.com>
2023-01-26 09:32:21 +0100

    Update createSchema.js

--- scripts/helpers/createSchema.js
+++ scripts/helpers/createSchema.js
@@ -29,7 +29,7 @@ module.exports.createSchema = (name, pascalName, schemaName, options) => {
     `import ${schemaImportName} from './${options.schemaDir}/${schemaName}';`,
     ...lines,
   ];
-  const fromNeedle = options.translatable ? `translateFields([` : `.concat([`;
+  const fromNeedle = options.translatable ? `translateFields([` : `...[`;
   const toNeedle = options.translatable ? `]),` : `])`;
 
   lines = addLine(`    ${schemaImportName},`, lines, fromNeedle);
