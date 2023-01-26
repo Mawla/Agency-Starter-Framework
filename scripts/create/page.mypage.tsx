703811173c92d0286c9800cb95146baf7916962c
Arjen <aratramba@gmail.com>
2023-01-26 11:43:57 +0100

    Update page.mypage.tsx

--- scripts/create/page.mypage.tsx
+++ scripts/create/page.mypage.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import { baseLanguage } from "../../../languages";
+import { SchemaName } from "../../../types.sanity";
+import { DocumentIcon } from "../../utils/DocumentIcon";
+import { ORDER_PUBLISHED_DESC, pageBase, PUBLISHED_AT_FIELD } from "./_page";
+import React from "react";
+import { defineType } from "sanity";
 
-import { baseLanguage } from '../../../languages';
-import { SchemaName } from '../../../types.sanity';
-import { DocumentIcon } from '../../utils/DocumentIcon';
-import { ORDER_PUBLISHED_DESC, pageBase, PUBLISHED_AT_FIELD } from './_page';
+export const SCHEMA_NAME: SchemaName = "MyPageSchema";
 
-export const SCHEMA_NAME: SchemaName = 'MyPageSchema';
-
-export default {
+export default defineType({
   name: SCHEMA_NAME,
-  title: 'MyPageTitle',
-  type: 'document',
-  singleton: true,
+  title: "MyPageTitle",
+  type: "document",
   orderings: [ORDER_PUBLISHED_DESC],
   preview: {
     select: {
       title: `title.${baseLanguage}`,
-      media: 'hero.0.visual.image1',
+      media: "hero.0.visual.image",
     },
   },
   icon: () => <DocumentIcon type="page" />,
@@ -25,4 +24,4 @@ export default {
   },
   fieldsets: [...pageBase.fieldsets],
   fields: [...pageBase.fields, PUBLISHED_AT_FIELD],
-};
+});
