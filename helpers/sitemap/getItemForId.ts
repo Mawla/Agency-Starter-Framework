d7f866d0bdf620ea7c6b6b6863b71fa75d6c1751
Arjen <aratramba@gmail.com>
2023-01-30 11:40:33 +0100

    remove unused helpers

--- helpers/sitemap/getItemForId.ts
+++
@@ -1,8 +0,0 @@
-import { SitemapItemType, SitemapType } from "../../queries/sitemap";
-
-export const getItemForId = (
-  id: string,
-  sitemap: SitemapType
-): SitemapItemType | null => {
-  return sitemap?.find((item) => item?._id === id) ?? null;
-};
