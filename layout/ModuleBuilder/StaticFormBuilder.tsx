a1502b03ca59868729d5b5e098df1e93677a84b8
Arjen <aratramba@gmail.com>
2023-01-31 15:44:57 +0100

    Update StaticFormBuilder.tsx

--- layout/ModuleBuilder/StaticFormBuilder.tsx
+++ layout/ModuleBuilder/StaticFormBuilder.tsx
@@ -1,21 +1,19 @@
-
-import React, { lazy, useState } from 'react';
-import { Suspense } from 'react';
-
-import { PortableTextProps } from '../../components/content/PortableText';
+import { PortableTextProps } from "../../components/content/PortableText";
 import {
-  type NewsLetterSignUpFormProps,
-  type NewsLetterSignUpFormValues,
-} from '../../forms/NewsLetterSignUpForm';
-import { StaticFormApiBody } from '../../pages/api/static-form';
+  NewsLetterSignUpFormProps,
+  NewsLetterSignUpFormValues,
+} from "../../forms/NewsLetterSignUpForm";
+import { StaticFormApiBody } from "../../pages/api/static-form";
 import {
   StaticFormOptionsType,
   StaticFormOptionType,
   StaticFormType,
   SuccessOrErrorMessage,
-} from '../../types';
-import { LazyLoadInView } from './LazyLoadInView';
-import ModuleErrorBoundary from './ModuleErrorBoundary';
+} from "../../types";
+import { LazyLoadInView } from "./LazyLoadInView";
+import ModuleErrorBoundary from "./ModuleErrorBoundary";
+import React, { lazy, useState, ComponentType } from "react";
+import { Suspense } from "react";
 
 export type StaticFormBuilderProps = {
   name?: string;
@@ -29,18 +27,20 @@ export type StaticFormBuilderProps = {
 const PortableText = lazy<React.ComponentType<PortableTextProps>>(
   () =>
     import(
-      /* webpackChunkName: "PortableText" */ '../../components/content/PortableText'
+      /* webpackChunkName: "PortableText" */ "../../components/content/PortableText"
     ) as any,
 );
 
-const NewsLetterSignUpForm = lazy<React.ComponentType<NewsLetterSignUpFormProps>>(
+const NewsLetterSignUpForm = lazy<
+  React.ComponentType<NewsLetterSignUpFormProps>
+>(
   () =>
     import(
-      /* webpackChunkName: "NewsLetterSignUpForm" */ '../../forms/NewsLetterSignUpForm'
-    ) ,
+      /* webpackChunkName: "NewsLetterSignUpForm" */ "../../forms/NewsLetterSignUpForm"
+    ),
 );
 
-export type StaticFormState = 'loading' | 'idle' | 'success' | 'error';
+export type StaticFormState = "loading" | "idle" | "success" | "error";
 
 export const StaticFormBuilder = ({
   name,
@@ -50,7 +50,7 @@ export const StaticFormBuilder = ({
   error,
   className,
 }: StaticFormBuilderProps) => {
-  const [state, setState] = useState<StaticFormState>('idle');
+  const [state, setState] = useState<StaticFormState>("idle");
   const [result, setResult] = useState<string | null>(null);
 
   if (!formId) return null;
@@ -66,7 +66,7 @@ export const StaticFormBuilder = ({
    */
 
   const handleSubmit = async (values: NewsLetterSignUpFormValues) => {
-    setState('loading');
+    setState("loading");
     setResult(null);
 
     const body: StaticFormApiBody = {
@@ -76,16 +76,16 @@ export const StaticFormBuilder = ({
     };
 
     const res = await fetch(`/api/static-form`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(body),
     });
 
     const result: SuccessOrErrorMessage = await res.json();
-    if ('success' in result) {
-      setState('success');
+    if ("success" in result) {
+      setState("success");
       return setResult(result.success);
     }
-    setState('error');
+    setState("error");
     setResult(result.error);
   };
 
@@ -102,8 +102,8 @@ export const StaticFormBuilder = ({
       <Suspense fallback={``}>
         <ModuleErrorBoundary>
           <LazyLoadInView>
-            {formId === 'newsletter-sign-up' && (
-              <NewsLetterSignUpForm {...formProps as any} />
+            {formId === "newsletter-sign-up" && (
+              <NewsLetterSignUpForm {...(formProps as any)} />
             )}
           </LazyLoadInView>
         </ModuleErrorBoundary>
