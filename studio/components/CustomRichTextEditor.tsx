b7d8bbe4ea7d603e20a3404fe33637c074d588c0
Arjen <aratramba@gmail.com>
2023-01-31 09:17:47 +0100

    Update CustomRichTextEditor.tsx

--- studio/components/CustomRichTextEditor.tsx
+++ studio/components/CustomRichTextEditor.tsx
@@ -1,54 +1,54 @@
-// CustomRichTextEditor.js
-import {getBlockContentFeatures, htmlToBlocks, normalizeBlock} from '@sanity/block-tools'
-import {PortableTextInput} from 'sanity'
-import React, {ComponentType, useRef} from 'react'
+import { htmlToBlocks } from "@sanity/block-tools";
+import { nanoid } from "nanoid";
+import React, { ComponentType } from "react";
+import { PortableTextInput } from "sanity";
 
-import {nanoid} from 'nanoid'
-
-export const CustomRichTextEditor: ComponentType<any> = React.forwardRef((props, ref) => {
-  return <PortableTextInput {...props} onPaste={handlePaste} />
-})
+export const CustomRichTextEditor: ComponentType<any> = React.forwardRef(
+  (props, ref) => {
+    return <PortableTextInput {...props} onPaste={handlePaste} />;
+  },
+);
 
 function handlePaste(props: {
-  event: React.ClipboardEvent
-  type: {[key: string]: any}
-  value: any[]
-  path: any[]
+  event: React.ClipboardEvent;
+  type: { [key: string]: any };
+  value: any[];
+  path: any[];
 }) {
-  const {event, type, path} = props
+  const { event, type, path } = props;
 
-  const html = event.clipboardData.getData('text/html')
+  const html = event.clipboardData.getData("text/html");
 
   if (html) {
     const blocks = htmlToBlocks(html, type as any, {
       rules: [
         {
           deserialize(el: Node, next, block) {
-            let element = el as HTMLElement
-            if (element?.tagName?.toLowerCase() != 'a') return undefined
+            let element = el as HTMLElement;
+            if (element?.tagName?.toLowerCase() != "a") return undefined;
 
-            const href = element.getAttribute('href')
+            const href = element.getAttribute("href");
 
             if (!href) {
-              return next(el.childNodes)
+              return next(el.childNodes);
             }
 
             const markDef = {
               _key: nanoid(),
-              _type: 'link',
+              _type: "link",
               external: href,
-            }
+            };
             return {
-              _type: '__annotation',
+              _type: "__annotation",
               markDef: markDef,
               children: next(el.childNodes),
-            }
+            };
           },
         },
       ],
-    })
+    });
     // return an insert patch
-    return {insert: blocks, path}
+    return { insert: blocks, path };
   }
-  return undefined
+  return undefined;
 }
