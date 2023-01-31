28fed6a122010d77d5042dd4dc15d160fbfed846
Arjen <aratramba@gmail.com>
2023-01-31 12:48:43 +0100

    Update preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,39 +1,11 @@
 import { PageContext } from "../context/PageContext";
 import { SiteContext } from "../context/SiteContext";
-import { SitemapItemType } from "../queries/sitemap";
 import "../styles/plyr-custom.css";
 import "../styles/plyr.css";
 import "../styles/styles.css";
-import { RouterContext } from "next/dist/shared/lib/router-context";
 import * as NextImage from "next/image";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  options: {
-    storySort: {
-      order: [
-        "Introduction",
-        "Framework",
-        "Templates",
-        "Components",
-        "Modules",
-      ],
-    },
-  },
-  nextRouter: {
-    Provider: RouterContext.Provider,
-    asPath: "/page1/page2/page3",
-    locale: "en",
-  },
-};
 
 const OriginalNextImage = NextImage.default;
 Object.defineProperty(NextImage, "default", {
@@ -42,60 +14,6 @@ Object.defineProperty(NextImage, "default", {
     <OriginalNextImage {...props} unoptimized loader={({ src }) => src} />
   ),
 });
-
-const DEMO_SITEMAP: SitemapItemType[] = [
-  {
-    _id: "xx",
-    _type: "page.content",
-    _updatedAt: "2022-01-04T14:26:24Z",
-    path: "/page1",
-    title: "Page 1",
-    paths: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-    titles: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-  },
-  {
-    _id: "xx",
-    _type: "page.content",
-    _updatedAt: "2022-01-04T15:03:13Z",
-    path: "/page1/page2",
-    title: "Page 2",
-    paths: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-    titles: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-  },
-  {
-    _id: "xxx",
-    _type: "page.content",
-    _updatedAt: "2022-01-04T15:03:13Z",
-    path: "/page1/page2/page3",
-    title: "Page 3",
-    paths: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-    titles: {
-      en: "/",
-      it: "/",
-      es: "/",
-    },
-  },
-];
 
 const queryClient = new QueryClient();
 
@@ -106,14 +24,30 @@ export const decorators = [
         <SiteContext.Provider
           value={{
             config: { general: {} },
-            sitemap: DEMO_SITEMAP,
           }}
         >
           <PageContext.Provider
             value={{
               isPreviewMode: false,
               language: "en",
-              sitemapItem: DEMO_SITEMAP[0],
+              sitemapItem: {
+                _id: "xx",
+                _type: "page.content",
+                _updatedAt: "2022-01-04T14:26:24Z",
+                path: "/page1",
+                title: "Page 1",
+                paths: {
+                  en: "/",
+                  it: "/",
+                  es: "/",
+                },
+                titles: {
+                  en: "/",
+                  it: "/",
+                  es: "/",
+                },
+              },
+              breadcrumb: [],
             }}
           >
             <Story />
