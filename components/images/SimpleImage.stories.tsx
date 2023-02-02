f238ea73d048c3208d137a924c986e4eb3b44f91
Arjen <aratramba@gmail.com>
2023-02-02 07:22:13 +0100

    Create SimpleImage.stories.tsx

---
+++ components/images/SimpleImage.stories.tsx
@@ -0,0 +1,19 @@
+import { demoImage2 } from "../../stories/content";
+import { SimpleImage } from "./SimpleImage";
+import { Meta } from "@storybook/react";
+import React from "react";
+
+export default {
+  component: SimpleImage,
+  title: "Components/SimpleImage",
+} as Meta;
+
+export const Default = () => (
+  <SimpleImage {...demoImage2} width={100} height={100} />
+);
+
+export const Fill = () => (
+  <div className="relative aspect-video border">
+    <SimpleImage {...demoImage2} priority loading="eager" fill />
+  </div>
+);
