4185b4600a6f726759867cbe423796ca24aac794
Arjen <aratramba@gmail.com>
2023-02-07 16:03:53 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /(bg|text|border)-(neutral|action)-(100|200|300|400|500|600|700|800|900)/,
+        /(bg|text|border)-(neutral|action|brand)-(100|200|300|400|500|600|700|800|900)/,
     },
     {
       pattern: /(bg|text|border)-(white|black)/,
