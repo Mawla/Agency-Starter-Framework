3eed8c873d6d51e256cc01999f6db2ee0e9d16de
Arjen <aratramba@gmail.com>
2023-02-07 15:04:09 +0100

    Update sitemap.ts

--- test/fixtures/sitemap.ts
+++ test/fixtures/sitemap.ts
@@ -7,19 +7,16 @@ export const DEMO_SITEMAP: SitemapType = [
     title: "content page",
     titles: {
       en: "Content page",
-      it: "Content pagina",
       es: "Content página",
     },
     path: "/page1",
     paths: {
       en: "/page1",
-      it: "/it/page1",
       es: "/es/page1",
     },
     _updatedAt: "xxx",
     excludeFromSitemap: {
       en: true,
-      it: true,
       es: true,
     },
   },
@@ -29,19 +26,16 @@ export const DEMO_SITEMAP: SitemapType = [
     title: "content page 2",
     titles: {
       en: "Content page 2",
-      it: "Content pagina 2",
       es: "Content página 2",
     },
     path: "/page1/page2",
     paths: {
       en: "/page1/page2",
-      it: "/it/page1/page2",
       es: "/es/page1/page2",
     },
     _updatedAt: "xxx",
     excludeFromSitemap: {
       en: true,
-      it: true,
       es: true,
     },
   },
@@ -51,19 +45,16 @@ export const DEMO_SITEMAP: SitemapType = [
     title: "content page 3",
     titles: {
       en: "Content page 3",
-      it: "Content pagina 3",
       es: "Content página 3",
     },
     path: "/page1/page2/page3",
     paths: {
       en: "/page1/page2/page3",
-      it: "/it/page1/page2/page3",
       es: "/es/page1/page2/page3",
     },
     _updatedAt: "xxx",
     excludeFromSitemap: {
       en: true,
-      it: true,
       es: true,
     },
   },
