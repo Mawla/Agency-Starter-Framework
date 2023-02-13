184a808dbf50c2915d55297b4696c2eb4cf53c56
Arjen <aratramba@gmail.com>
2023-02-13 08:58:57 +0100

    make home singleton

--- studio/schemas/documents/page.home.tsx
+++ studio/schemas/documents/page.home.tsx
@@ -10,6 +10,9 @@ export default defineType({
   name: SCHEMA_NAME,
   title: "Homepage",
   type: "document",
+  options: {
+    singleton: true,
+  },
   icon: () => <House weight="thin" size={20} />,
   preview: DEFAULT_CONTENT_PAGE_PREVIEW,
   fieldsets: [...pageBase.fieldsets],
