c54c4c38f8a9c115b3f84755a8d84d65ab936928
Arjen <aratramba@gmail.com>
2023-02-14 10:16:25 +0100

    refactor path helpers

--- helpers/sitemap/getPathForId.test.ts
+++ helpers/sitemap/getPathForId.test.ts
@@ -2,16 +2,11 @@ import { DEMO_SITEMAP } from "../../test/fixtures/sitemap";
 import { getPathForId } from "./getPathForId";
 
 test("test non-existent existing path", () => {
-  const path = getPathForId("foo", DEMO_SITEMAP, "en");
+  const path = getPathForId("foo", DEMO_SITEMAP);
   expect(path).toEqual("");
 });
 
 test("test existing path", () => {
-  const path = getPathForId("xxx3", DEMO_SITEMAP, "es");
+  const path = getPathForId("xxx3", DEMO_SITEMAP);
   expect(path).toEqual("/es/page1/page2/page3");
-});
-
-test("test existing path with wrong language", () => {
-  const path = getPathForId("/es/page1/page2/page3", DEMO_SITEMAP, "en");
-  expect(path).toEqual("");
 });
--- helpers/sitemap/getPathForId.ts
+++ helpers/sitemap/getPathForId.ts
@@ -1,10 +1,5 @@
-import { LanguageType } from "../../languages";
 import { SitemapType } from "../../queries/sitemap.query";
 
-export const getPathForId = (
-  id: string,
-  sitemap: SitemapType,
-  language: LanguageType,
-): string => {
-  return sitemap.find((item) => item?._id === id)?.paths[language] || "";
+export const getPathForId = (id: string, sitemap: SitemapType): string => {
+  return sitemap.find((item) => item?._id === id)?.path || "";
 };
