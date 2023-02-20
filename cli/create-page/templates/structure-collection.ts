98e7ef243d3d2d6549e1db1aeffdc2a817b932fb
Arjen <aratramba@gmail.com>
2023-02-20 10:17:57 +0100

    Update structure-collection.ts

--- cli/create-page/templates/structure-collection.ts
+++ cli/create-page/templates/structure-collection.ts
@@ -14,7 +14,7 @@ export const getStructureCollection = ({
   articleSchemaName,
 }: Props) => {
   return `S.listItem()
-    .title("${pageName} overview")
+    .title("${pageName}")
     .icon(getIconForSchema(S, "${schemaName}"))
     .child(
       list(S, { title: "${schemaName}" }).items([
