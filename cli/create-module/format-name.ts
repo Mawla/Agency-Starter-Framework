734bdfe4756a04e90c1d66f2e175b4f6c3f3e968
Arjen <aratramba@gmail.com>
2023-02-21 08:23:55 +0100

    Update format-name.ts

--- cli/create-module/format-name.ts
+++ cli/create-module/format-name.ts
@@ -1,9 +1,10 @@
 import { pascalCase } from "../utils/pascal-case";
 
 export function formatName(name: string) {
-  let pascalName = `${pascalCase(name)}`;
+  name = name.trim().replace(/\s\s+/g, "");
+  let pascalName = pascalCase(name);
   let lowerName = name.toLowerCase();
-  let schemaName = `module.${name.toLowerCase().replace(/\s/g, "")}`;
+  let schemaName = `module.${name.toLowerCase()}`;
 
   return {
     pascalName,
