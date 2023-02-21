f77cc7f2257680d883dcf0dd6c6ee719d1c85b7c
Arjen <aratramba@gmail.com>
2023-02-21 10:58:41 +0100

    Update options.ts

--- cli/create-module/templates/options.ts
+++ cli/create-module/templates/options.ts
@@ -3,12 +3,14 @@ export const getOptionsSnippet = () => {
     import { COLORS } from "../../colors";
     import { SIZE_OPTIONS } from "../../components/module/Title";
     import { pick } from "../../helpers/utils/object";
-    import { ALIGNMENTS } from "../../types";
 
     export const BACKGROUND_COLOR_OPTIONS = pick(COLORS, "white", "black");
     export type BackgroundColorType = keyof typeof BACKGROUND_COLOR_OPTIONS;
 
     export const TITLE_SIZE_OPTIONS = pick(SIZE_OPTIONS, "lg", "xl");
     export type TitleSizeType = keyof typeof TITLE_SIZE_OPTIONS;
+    
+    export const TITLE_COLOR_OPTIONS = pick(COLORS, "black", "white");
+    export type TitleColorType = keyof typeof TITLE_COLOR_OPTIONS;
     `;
 };
