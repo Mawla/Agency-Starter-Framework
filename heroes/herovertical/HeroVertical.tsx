8cb101f1445bbd29f1a56ab60c0121a2429b4819
Ben <ben@mawla.ie>
2023-02-22 14:38:40 +0000

    Add Bleed and Width component imports to HeroVertical

--- heroes/herovertical/HeroVertical.tsx
+++ heroes/herovertical/HeroVertical.tsx
@@ -1,6 +1,8 @@
 import { ResponsiveImageProps } from "../../components/images/ResponsiveImage";
+import { BleedProps } from "../../components/module/Bleed";
 import { TextProps } from "../../components/module/Text";
 import { TitleProps } from "../../components/module/Title";
+import { WidthProps } from "../../components/module/Width";
 import { WrapperProps } from "../../components/module/Wrapper";
 import { BackgroundColorType } from "../../components/module/background.options";
 import { SpaceType } from "../../components/module/spacing.options";
@@ -37,6 +39,20 @@ const ResponsiveImage = lazy<ComponentType<ResponsiveImageProps>>(
     ),
 );
 
+const Bleed = lazy<ComponentType<BleedProps>>(
+  () =>
+    import(
+      /* webpackChunkName: "BleedProps" */ "../../components/module/Bleed"
+    ),
+);
+
+const Width = lazy<ComponentType<WidthProps>>(
+  () =>
+    import(
+      /* webpackChunkName: "WidthProps" */ "../../components/module/Width"
+    ),
+);
+
 export type HeroVerticalProps = {
   theme?: {
     module?: {
@@ -66,38 +82,54 @@ export const HeroVertical = ({
   image,
 }: HeroVerticalProps) => {
   return (
-    <Wrapper
-      theme={{
-        ...theme?.module,
-      }}
-    >
-      {(title || eyebrow) && (
-        <div className="mb-4 md:mb-6">
-          <Title
-            size={theme?.title?.size || "lg"}
-            as={theme?.title?.level}
-            color={theme?.title?.color}
-            eyebrow={eyebrow}
-          >
-            {title}
-          </Title>
-        </div>
-      )}
+    <header className="relative z-0 overflow-hidden text-neutral-500">
+      <div className="relative flex flex-row items-center z-30 pt-10 md:pt-15 lg:pt-20">
+        <Bleed bleed="md">
+          <Width width="inner">
+            <div className="relative flex flex-col tablet:max-w-[75%] lg:max-w-[790px] gap-4 text-center mx-auto">
+              {(title || eyebrow) && (
+                <Title as="h1" size="6xl" color="neutral-800" eyebrow={eyebrow}>
+                  {title}
+                </Title>
+              )}
 
-      {intro && (
-        <div className="mb-10 md:mb-14">
-          <Text color={theme?.text?.color}>
-            <PortableText content={intro as any} />
-          </Text>
-        </div>
-      )}
+              {text && (
+                <Text
+                  size="2xl"
+                  className="mt-2"
+                  color="neutral-900"
+                  align="center"
+                >
+                  <PortableText content={text as any} />
+                </Text>
+              )}
 
-      {image && (
-        <div className="w-96 relative aspect-video">
-          <ResponsiveImage {...image} fill className="absolute inset-0" />
-        </div>
-      )}
-    </Wrapper>
+              {buttons && (
+                <ButtonGroup
+                  className="mt-4 md:mt-6 lg:mt-8 mx-auto"
+                  items={buttons}
+                  align="center"
+                />
+              )}
+            </div>
+
+            {image && (
+              <div className="relative block mx-auto overflow-hidden rounded-lg mt-10 aspect-video">
+                <ResponsiveImage
+                  {...image}
+                  priority
+                  loading="eager"
+                  roundSize={50}
+                  width={1374}
+                  height={774}
+                  fill
+                />
+              </div>
+            )}
+          </Width>
+        </Bleed>
+      </div>
+    </header>
   );
 };
 
