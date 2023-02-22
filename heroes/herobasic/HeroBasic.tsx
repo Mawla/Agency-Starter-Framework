2f1f914bdf0f97f9a4dfc3c62182e8725fd67f6f
Ben <ben@mawla.ie>
2023-02-22 11:11:49 +0000

    Convert width container to flexbox, center align elements

--- heroes/herobasic/HeroBasic.tsx
+++ heroes/herobasic/HeroBasic.tsx
@@ -63,8 +63,8 @@ export const HeroBasic = (data: HeroBasicProps) => {
     <header className="relative z-0 overflow-hidden text-neutral-500">
       <div className="relative flex flex-row items-center z-30 pt-10 md:pt-15 lg:pt-20">
         <Bleed bleed="md">
-          <Width width="inner">
-            <div className="relative flex flex-col tablet:max-w-[75%] lg:max-w-[790px] gap-4 text-center mx-auto">
+          <Width width="inner" className="flex flex-row">
+            <div className="relative flex flex-col tablet:max-w-[75%] lg:max-w-[790px] gap-4 mx-auto">
               {(title || eyebrow) && (
                 <Title as="h1" size="6xl" color="neutral-800" eyebrow={eyebrow}>
                   {title}
@@ -72,22 +72,13 @@ export const HeroBasic = (data: HeroBasicProps) => {
               )}
 
               {text && (
-                <Text
-                  size="2xl"
-                  className="mt-2"
-                  color="neutral-900"
-                  align="center"
-                >
+                <Text size="2xl" className="mt-2" color="neutral-900">
                   <PortableText content={text as any} />
                 </Text>
               )}
 
               {buttons && (
-                <ButtonGroup
-                  className="mt-4 md:mt-6 lg:mt-8 mx-auto"
-                  items={buttons}
-                  align="center"
-                />
+                <ButtonGroup className="mt-4 md:mt-6 lg:mt-8" items={buttons} />
               )}
             </div>
 
