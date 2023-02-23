22d9170c2db5dca79625415586720a292fb78e3f
Arjen <aratramba@gmail.com>
2023-02-23 07:57:32 +0100

    remove cookie pro

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
-const shouldEnableMarker = () =>
-  process.env.NEXT_PUBLIC_SANITY_DATASET === "client-staging";
-
 class MyDocument extends Document {
   render() {
     return (
@@ -54,33 +51,6 @@ class MyDocument extends Document {
             as="image"
             media="(max-width: 1024px)"
           />
-
-          {/* {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
-            <>
-              <link rel="preconnect" href="https://www.google-analytics.com" />
-              <link rel="preconnect" href="https://www.googletagmanager.com" />
-              <link rel="preconnect" href="https://consent.cookiebot.com" />
-              <link
-                rel="preload"
-                href="https://cookie-cdn.cookiepro.com/scripttemplates/6.27.0/otBannerSdk.js"
-                as="script"
-              />
-            </>
-          )}
-          {shouldEnableMarker() && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `window.markerConfig = { destination: '6194cb5ce8a98103607e6fb0' };`,
-              }}
-            />
-          )}
-          {shouldEnableMarker() && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);`,
-              }}
-            />
-          )} */}
         </Head>
         <body>
           <Main />
