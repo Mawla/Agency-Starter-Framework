4093e611cc1a255e3a69c77749d3b7c902fb90d1
Arjen <aratramba@gmail.com>
2023-06-28 15:53:59 +0200

    add demo images

--- stories/content.ts
+++ stories/content.ts
@@ -1,64 +1,29 @@
 import { ImageType } from "../types";
 
 export const demoImage: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/8176ab31464cc5bc14927e66e0f52433da2a51ea-880x528.jpg",
+  src: "https://cdn.sanity.io/images/lra7ykt5/development/8176ab31464cc5bc14927e66e0f52433da2a51ea-880x528.jpg",
   alt: "demoimage",
   width: 880,
   height: 528,
 };
 
 export const demoImage2: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/22a209cca6c9eccce6c2619d2f6ecee93c419336-1296x893.jpg",
+  src: "https://cdn.sanity.io/images/lra7ykt5/development/22a209cca6c9eccce6c2619d2f6ecee93c419336-1296x893.jpg",
   alt: "demoimage2",
   width: 1296,
   height: 893,
 };
 
 export const demoImage3: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/7eaa67f5bb42ecf6d53e5949f32f898e1c4f16ae-800x1200.webp",
+  src: "https://cdn.sanity.io/images/lra7ykt5/development/7eaa67f5bb42ecf6d53e5949f32f898e1c4f16ae-800x1200.webp",
   alt: "demoimage3",
   width: 800,
   height: 1200,
 };
 
 export const demoImage4: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/ec6812fa75d044ea29ee2a8c1a09552847bc17d9-970x546.webp",
+  src: "https://cdn.sanity.io/images/lra7ykt5/development/f008ee1237df81834de2329e6e74d9e693962a85-970x546.webp",
   alt: "demoimage4",
   width: 970,
   height: 546,
-};
-
-export const demoImage5: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/2ded02f57b77592eb75de390ab4c69f99704490f-5184x3456.jpg",
-  alt: "demoimage5",
-  width: 5184,
-  height: 3456,
-  hotspot: {
-    _type: "sanity.imageHotspot",
-    width: 1,
-    height: 1,
-    x: 1,
-    y: 0.00001,
-  },
-};
-
-export const demoImage6: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/91077ecc556ad9ff49921a000f2c10951d2dfd58-5184x3456.jpg",
-  alt: "demoimage4",
-  width: 5184,
-  height: 3456,
-};
-
-export const demoImage7: ImageType = {
-  src: "https://cdn.sanity.io/images/vs4fnw8m/development/41c317c22bc54fca0532381cb4070690f8f3d0e0-5184x3456.jpg",
-  alt: "demoimage4",
-  width: 5184,
-  height: 3456,
-  hotspot: {
-    _type: "sanity.imageHotspot",
-    width: 1,
-    height: 1,
-    x: 0.5,
-    y: 1,
-  },
 };
