621ba49113f101db5674456822d9b9f286a4095a
Ben <ben@mawla.ie>
2023-06-29 10:33:59 +0100

    Correctly render items inside grid columns

--- blocks/block2/Block2.tsx
+++ blocks/block2/Block2.tsx
@@ -5,6 +5,7 @@ import { BackgroundColorType } from "../../components/block/background.options";
 import { SpaceType } from "../../components/block/spacing.options";
 import { ButtonProps } from "../../components/buttons/Button";
 import { ButtonGroupProps } from "../../components/buttons/ButtonGroup";
+import { ResponsiveImageProps } from "../../components/images/ResponsiveImage";
 import { PortableTextProps } from "../../components/portabletext/PortableText";
 import { HeadingLevelType, ImageType } from "../../types";
 import {
@@ -38,6 +39,13 @@ const ButtonGroup = lazy<ComponentType<ButtonGroupProps>>(
   () =>
     import(
       /* webpackChunkName: "ButtonGroup" */ "../../components/buttons/ButtonGroup"
+    )
+);
+
+const ResponsiveImage = lazy<ComponentType<ResponsiveImageProps>>(
+  () =>
+    import(
+      /* webpackChunkName: "ResponsiveImageProps" */ "../../components/images/ResponsiveImage"
     )
 );
 
@@ -106,13 +114,11 @@ export const Block2 = ({
 
           <div className="p-4 mt-8 rounded-lg sm:p-12 lg:mt-16 bg-gray-50 dark:bg-gray-800">
             <div className="grid grid-cols-1 gap-8 sm:gap-12 lg:grid-cols-2">
-              {items && Boolean(items?.filter(Boolean).length) && (
-                <ul className="pt-8">
-                  {items?.map((item) => {
-                    return <Item key={item._key} {...item} />;
-                  })}
-                </ul>
-              )}
+              {items &&
+                Boolean(items?.filter(Boolean).length) &&
+                items?.map((item) => {
+                  return <Item key={item._key} {...item} />;
+                })}
             </div>
           </div>
 
@@ -139,26 +145,9 @@ const Item = ({ title, intro, image, _key }: ItemProps) => {
   return (
     <div className="flex flex-col items-start gap-4 sm:gap-5 sm:flex-row">
       <div className="bg-gray-100 dark:bg-gray-700 rounded-full w-16 h-16 lg:w-24 lg:h-24 flex items-center justify-center shrink-0">
-        <svg
-          aria-hidden="true"
-          className="w-10 lg:w-12 h-10 lg:h-12 text-primary-600 dark:text-primary-500"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-          ></path>
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-          ></path>
-        </svg>
+        <div className="w-96 relative aspect-video">
+          <ResponsiveImage {...image} fill className="absolute inset-0" />
+        </div>
       </div>
       <div>
         {title && (
