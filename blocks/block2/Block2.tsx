c2c8b8e4b6a18fccaec3732ac80c4dd2d62109d9
Ben <ben@mawla.ie>
2023-06-29 10:22:55 +0100

    Replace <a> tag with ButtonGroup

--- blocks/block2/Block2.tsx
+++ blocks/block2/Block2.tsx
@@ -117,36 +117,14 @@ export const Block2 = ({
           </div>
 
           <div className="mt-8 text-center lg:mt-16">
-            <a
-              href="#"
-              title=""
-              className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              role="button"
-            >
-              Get a demo
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 ml-2 -mr-1"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
+            {buttons && Boolean(buttons?.filter(Boolean).length) && (
+              <div className="mt-8 lg:mt-12">
+                <ButtonGroup items={buttons} />
+              </div>
+            )}
           </div>
         </div>
       </section>
-
-      {buttons && Boolean(buttons?.filter(Boolean).length) && (
-        <div className="mt-8 lg:mt-12">
-          <ButtonGroup items={buttons} />
-        </div>
-      )}
     </Wrapper>
   );
 };
