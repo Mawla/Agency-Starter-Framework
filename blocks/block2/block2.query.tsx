225e2eee160238808125aa2faa3b14fdd4565157
Ben <ben@mawla.ie>
2023-06-29 09:35:47 +0100

    Add intro and image to items[] query for Block2

--- blocks/block2/block2.query.tsx
+++ blocks/block2/block2.query.tsx
@@ -2,6 +2,7 @@ import { buttonQuery } from "../../components/buttons/button.query";
 import { richTextQuery } from "../../components/portabletext/portabletext.query";
 import { LanguageType } from "../../languages";
 import groq from "groq";
+import {imageQuery} from "../../components/images/image.query";
 
 export const getBlock2Query = (language: LanguageType) => groq`
     _type == "block.block2" => {
@@ -10,5 +11,10 @@ export const getBlock2Query = (language: LanguageType) => groq`
       title,
       intro[] ${richTextQuery},
       buttons[] ${buttonQuery},
-      items[] { _key, title },
+      items[] { 
+        _key, 
+        title,
+        intro[] ${richTextQuery},
+        "image": ${imageQuery},
+      },
     }`;
