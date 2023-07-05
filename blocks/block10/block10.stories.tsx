150e2297717c4aac1ca4c210547ad584f295aa93
Arjen <aratramba@gmail.com>
2023-07-05 15:25:41 +0200

    Update block10.stories.tsx

--- blocks/block10/block10.stories.tsx
+++ blocks/block10/block10.stories.tsx
@@ -29,6 +29,45 @@ const DEMO_CONTENT: Block10Props = {
       Ask us anything about our brand and products, and get factual responses.
     </p>
   ),
+
+  faq: [
+    {
+      title: "What is the best thing about Switzerland?",
+      content: [
+        {
+          _key: "1bc91c6df8b3",
+          _type: "block",
+          children: [
+            {
+              _key: "9dc1274a39d5",
+              _type: "span",
+              marks: [],
+              text: "We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.",
+            },
+          ],
+          markDefs: [],
+          style: "normal",
+        },
+      ],
+    },
+    {
+      title:
+        "Did you hear about the mathematician who's afraid of negative numbers?",
+      content: <p>He'll stop at nothing to avoid them!</p>,
+    },
+    {
+      title: "Hear about the new restaurant called Karma?",
+      content: <p>There’s no menu: You get what you deserve.</p>,
+    },
+    {
+      title: "Did you hear about the actor who fell through the floorboards?",
+      content: <p>He was just going through a stage.</p>,
+    },
+    {
+      title: "Did you hear about the claustrophobic astronaut?",
+      content: <p>He just needed a little space.</p>,
+    },
+  ],
   buttons: [{ label: "Read more" }],
 };
 
