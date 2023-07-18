a6c4be7e2dff14bf6b4560de484e9392ff8eb211
Arjen <aratramba@gmail.com>
2023-07-18 14:03:18 +0200

    remove manrope

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/styles.css";
-import { Manrope } from "@next/font/google";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
@@ -8,11 +7,6 @@ import "tailwindcss/tailwind.css";
 
 const queryClient = new QueryClient();
 
-const manrope = Manrope({
-  subsets: ["latin"],
-  variable: "--font-manrope",
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
@@ -20,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <div className={`${manrope.variable} font-sans`}>
+      <div className="font-sans">
         <Component {...pageProps} />
       </div>
     </QueryClientProvider>
