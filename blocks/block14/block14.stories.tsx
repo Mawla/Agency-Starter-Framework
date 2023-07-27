12784c29231f2641ecc866a81b3b0dfb570c9e3b
Arjen <aratramba@gmail.com>
2023-07-27 17:07:18 +0200

    Update block14.stories.tsx

--- blocks/block14/block14.stories.tsx
+++ blocks/block14/block14.stories.tsx
@@ -1,18 +1,5 @@
-import {
-  TextSizeType,
-  TEXT_SIZE_OPTIONS,
-} from "../../components/text/text.options";
-import {
-  TitleSizeType,
-  TITLE_SIZE_OPTIONS,
-} from "../../components/title/title.options";
-import { demoImage } from "../../stories/content";
 import { COLORS } from "../../theme";
-import {
-  ColorType,
-  HorizontalAlignType,
-  HORIZONTAL_ALIGN_OPTIONS,
-} from "../../types";
+import { ColorType } from "../../types";
 import { Block14, Block14Props } from "./Block14";
 import { Meta } from "@storybook/react";
 import React from "react";
@@ -24,7 +11,7 @@ export default {
 
 const DEMO_CONTENT: Block14Props = {
   title: "title",
-  intro: <p>intro</p>,
+  body: <p>intro</p>,
 };
 
 export const Default = () => <Block14 {...DEMO_CONTENT} />;
@@ -37,83 +24,6 @@ export const BlockBackgrounds = () => (
           {...DEMO_CONTENT}
           theme={{
             block: { background: color },
-          }}
-        />
-      </div>
-    ))}
-  </>
-);
-
-export const Alignments = () => (
-  <>
-    {(Object.keys(HORIZONTAL_ALIGN_OPTIONS) as HorizontalAlignType[]).map(
-      (align) => (
-        <div key={align}>
-          <Block14
-            {...DEMO_CONTENT}
-            theme={{
-              block: { align },
-            }}
-          />
-        </div>
-      ),
-    )}
-  </>
-);
-
-export const TitleColors = () => (
-  <>
-    {(Object.keys(COLORS) as ColorType[]).map((color) => (
-      <div key={color}>
-        <Block14
-          title={DEMO_CONTENT.title}
-          theme={{
-            title: { color },
-          }}
-        />
-      </div>
-    ))}
-  </>
-);
-
-export const TitleSizes = () => (
-  <>
-    {(Object.keys(TITLE_SIZE_OPTIONS) as TitleSizeType[]).map((size) => (
-      <div key={size}>
-        <Block14
-          title={DEMO_CONTENT.title}
-          theme={{
-            title: { size },
-          }}
-        />
-      </div>
-    ))}
-  </>
-);
-
-export const IntroColors = () => (
-  <>
-    {(Object.keys(COLORS) as ColorType[]).map((color) => (
-      <div key={color}>
-        <Block14
-          intro={DEMO_CONTENT.intro}
-          theme={{
-            intro: { color },
-          }}
-        />
-      </div>
-    ))}
-  </>
-);
-
-export const IntroSizes = () => (
-  <>
-    {(Object.keys(TEXT_SIZE_OPTIONS) as TextSizeType[]).map((size) => (
-      <div key={size}>
-        <Block14
-          intro={DEMO_CONTENT.intro}
-          theme={{
-            intro: { size },
           }}
         />
       </div>
