4ec23f0822cdc80643a92de5194b643926665748
Arjen <aratramba@gmail.com>
2023-07-31 11:02:56 +0200

    fix timezone

--- components/date/DateDisplay.test.tsx
+++ components/date/DateDisplay.test.tsx
@@ -7,51 +7,49 @@ jest.mock("next/dist/client/router", () => require("next-router-mock"));
 describe("DateDisplay", () => {
   it("renders default datetime", async () => {
     await act(() => {
-      render(<DateDisplay locale="en" datetime="2023-07-20T07:36:00.000Z" />);
+      render(<DateDisplay datetime="2023-07-20T07:36:00.000Z" />);
     });
     expect(screen.getByText("July 20, 2023")).toBeInTheDocument();
   });
 
   it("renders from date", async () => {
     await act(() => {
-      render(<DateDisplay locale="en" from="2023-07-20T07:36:00.000Z" />);
+      render(<DateDisplay from="2023-07-20T07:36:00.000Z" />);
     });
-    expect(screen.getByText("July 20, 2023 at 09:36")).toBeInTheDocument();
+    expect(screen.getByText("July 20, 2023 at 07:36")).toBeInTheDocument();
   });
 
   it("renders to date", async () => {
     await act(() => {
-      render(<DateDisplay locale="en" to="2023-07-20T07:36:00.000Z" />);
+      render(<DateDisplay to="2023-07-20T07:36:00.000Z" />);
     });
-    expect(screen.getByText("July 20, 2023 at 09:36")).toBeInTheDocument();
+    expect(screen.getByText("July 20, 2023 at 07:36")).toBeInTheDocument();
   });
 
   it("renders from to", async () => {
     await act(() => {
       render(
         <DateDisplay
-          locale="en"
           from="2022-07-20T07:36:00.000Z"
           to="2023-07-20T07:36:00.000Z"
         />,
       );
     });
-    expect(screen.getByText("July 20, 2022 at 09:36")).toBeInTheDocument();
-    expect(screen.getByText("July 20, 2023 at 09:36")).toBeInTheDocument();
+    expect(screen.getByText("July 20, 2022 at 07:36")).toBeInTheDocument();
+    expect(screen.getByText("July 20, 2023 at 07:36")).toBeInTheDocument();
   });
 
   it("renders from to same date", async () => {
     await act(() => {
       render(
         <DateDisplay
-          locale="en"
           from="2023-07-20T07:00:00.000Z"
           to="2023-07-20T07:36:00.000Z"
         />,
       );
     });
     expect(
-      screen.getByText("July 20, 2023 at 09:00 – 09:36"),
+      screen.getByText("July 20, 2023 at 07:00 – 07:36"),
     ).toBeInTheDocument();
   });
 });
