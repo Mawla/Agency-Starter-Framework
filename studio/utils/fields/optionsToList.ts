d36aecdead18f7fcbe1387c213c8e9cee23e83fb
Arjen <aratramba@gmail.com>
2023-08-07 14:25:14 +0200

    add sorting to options list

--- studio/utils/fields/optionsToList.ts
+++ studio/utils/fields/optionsToList.ts
@@ -1,7 +1,20 @@
-export const optionsToList = (options: {
-  [key: string]: string;
-}): { title: string; value: string }[] =>
-  Object.entries(options).map(([key, value]) => ({
+export const optionsToList = (
+  options: {
+    [key: string]: string;
+  },
+  sortNumeric = false,
+): { title: string; value: string }[] => {
+  const collator = new Intl.Collator([], { numeric: true });
+  let entries = Object.entries(options);
+
+  if (sortNumeric) {
+    entries = entries.sort((a, b) => {
+      return collator.compare(a[1], b[1]);
+    });
+  }
+
+  return entries.map(([key, value]) => ({
     title: value,
     value: key,
   }));
+};
