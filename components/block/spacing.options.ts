1ae85369fb8004d87b44583a6bce8b4693bf43be
Arjen <aratramba@gmail.com>
2023-08-16 11:53:47 +0200

    add 2xl spacing

--- components/block/spacing.options.ts
+++ components/block/spacing.options.ts
@@ -10,6 +10,7 @@ export const SPACE_OPTIONS = pick(
   "md",
   "lg",
   "xl",
+  "2xl",
 );
 export type SpaceTopType = keyof typeof SPACE_OPTIONS;
 export type SpaceBottomType = keyof typeof SPACE_OPTIONS;
@@ -27,6 +28,7 @@ export const paddingTopClasses: Record<SpaceTopType, string> = {
   md: "pt-10 sm:pt-10 md:pt-20 lg:pt-25",
   lg: "pt-12 sm:pt-12 md:pt-24 lg:pt-30",
   xl: "pt-16 sm:pt-16 md:pt-30 lg:pt-40",
+  "2xl": "pt-24 sm:pt-24 md:pt-40 lg:pt-60",
 };
 
 export const paddingBottomClasses: Record<SpaceBottomType, string> = {
@@ -37,6 +39,7 @@ export const paddingBottomClasses: Record<SpaceBottomType, string> = {
   md: "pb-10 sm:pb-10 md:pb-20 lg:pb-25",
   lg: "pb-12 sm:pb-12 md:pb-24 lg:pb-30",
   xl: "pb-16 sm:pb-16 md:pb-30 lg:pb-40",
+  "2xl": "pb-24 sm:pb-24 md:pb-40 lg:pb-60",
 };
 
 export const marginTopClasses: Record<SpaceTopType, string> = {
@@ -47,6 +50,7 @@ export const marginTopClasses: Record<SpaceTopType, string> = {
   md: "mt-10 sm:mt-10 md:mt-20 lg:mt-25",
   lg: "mt-12 sm:mt-12 md:mt-24 lg:mt-30",
   xl: "mt-16 sm:mt-16 md:mt-30 lg:mt-40",
+  "2xl": "mt-24 sm:mt-24 md:mt-40 lg:mt-60",
 };
 
 export const marginBottomClasses: Record<SpaceBottomType, string> = {
@@ -57,4 +61,5 @@ export const marginBottomClasses: Record<SpaceBottomType, string> = {
   md: "mb-10 sm:mb-10 md:mb-20 lg:mb-25",
   lg: "mb-12 sm:mb-12 md:mb-24 lg:mb-30",
   xl: "mb-16 sm:mb-16 md:mb-30 lg:mb-40",
+  "2xl": "mb-24 sm:mb-24 md:mb-40 lg:mb-60",
 };
