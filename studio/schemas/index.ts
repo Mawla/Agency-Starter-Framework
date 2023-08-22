c42ce8613b93dffaebb23daf3a68fa6e67e2faca
Arjen <aratramba@gmail.com>
2023-08-22 15:37:27 +0200

    delete block 11 testimonials

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -3,7 +3,6 @@ import block1 from "../../blocks/block1/block1.schema";
 import block2 from "../../blocks/block2/block2.schema";
 import block4 from "../../blocks/block4/block4.schema";
 import block10 from "../../blocks/block10/block10.schema";
-import block11 from "../../blocks/block11/block11.schema";
 import block12 from "../../blocks/block12/block12.schema";
 import block13 from "../../blocks/block13/block13.schema";
 import block14 from "../../blocks/block14/block14.schema";
@@ -86,7 +85,6 @@ export const schemaTypes = [
     block0,
     block1,
     block10,
-    block11,
     block12,
     block13,
     block14,
