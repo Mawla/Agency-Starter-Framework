b55cdbe9540ad6bde6ccce25691689c8fffedb22
Arjen <aratramba@gmail.com>
2023-09-21 10:12:26 +0200

    add smaller icon height

--- components/cards/composablecard.options.ts
+++ components/cards/composablecard.options.ts
@@ -1,5 +1,13 @@
 import { pick } from "../../helpers/utils/object";
-import { RATIOS, SIZES } from "../../types";
+import { SIZES } from "../../types";
 
-export const IMAGE_HEIGHT_OPTIONS = pick(SIZES, "xs", "sm", "md", "lg", "xl");
+export const IMAGE_HEIGHT_OPTIONS = pick(
+  SIZES,
+  "2xs",
+  "xs",
+  "sm",
+  "md",
+  "lg",
+  "xl",
+);
 export type ImageHeightType = keyof typeof IMAGE_HEIGHT_OPTIONS;
