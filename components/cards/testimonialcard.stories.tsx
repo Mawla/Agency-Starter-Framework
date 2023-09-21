000732a4132966b415bb0ef912e3925545a40e32
Arjen <aratramba@gmail.com>
2023-09-21 12:01:48 +0200

    add max width to testimonial card

--- components/cards/testimonialcard.stories.tsx
+++ components/cards/testimonialcard.stories.tsx
@@ -12,6 +12,47 @@ export const Card = () => (
   <TestimonialCard {...demoTestimonialsList[0]} type="card.testimonial" />
 );
 
+export const Poster = () => (
+  <TestimonialCard
+    {...demoTestimonialsList[0]}
+    type="card.testimonial"
+    theme={{
+      card: {
+        align: "center",
+        background: "white",
+        columns: 3,
+        paddingBottom: "20",
+        paddingTop: "20",
+        paddingX: "8",
+      },
+      border: {
+        width: 1,
+      },
+      title: {
+        weight: "medium",
+        font: "heading",
+        as: "h2",
+        size: "6xl",
+      },
+      content: {
+        size: "2xl",
+        weight: "medium",
+        font: "text",
+      },
+      jobTitle: {
+        font: "text",
+        size: "lg",
+        weight: "normal",
+      },
+      name: {
+        size: "lg",
+        weight: "medium",
+        font: "text",
+      },
+    }}
+  />
+);
+
 export const Theme = () => (
   <TestimonialCard
     type="card.testimonial"
