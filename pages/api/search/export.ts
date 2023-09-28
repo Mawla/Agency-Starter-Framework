f77aac92dbaa3d468a40b4d317280885f9190312
Arjen <aratramba@gmail.com>
2023-09-28 09:39:38 +0200

    create test for export query

--- pages/api/search/export.ts
+++ pages/api/search/export.ts
@@ -1,7 +1,5 @@
-import { getSitemapQuery } from "../../../queries/sitemap.query";
-import { LINKABLE_SCHEMAS } from "../../../types.sanity";
 import { sanityClient } from "../sanity-client";
-import groq from "groq";
+import { getExportQuery } from "./export.query";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type ResponseData = {
@@ -22,90 +20,7 @@ export default async function handler(
   res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
 
   const allPages = await sanityClient.fetch(
-    groq`
-    {
-      "sitemap": ${getSitemapQuery()},
-      "domain": *[_id == 'config_general'][0]{ domain }.domain
-    } {
-      sitemap,
-      domain,
-      "page": *[
-          !(_id in path("drafts.**")) 
-          && _type in [${Object.keys(LINKABLE_SCHEMAS)
-            .map((schema) => `'${schema}'`)
-            .join(", ")}]
-          && locked != true 
-          && seo.excludeFromSitemap != true
-          && defined(blocks)
-          && count(blocks) > 0
-          ${id ? `&& _id == '${id}'` : ""}
-          ${type ? `&& _type == '${type}'` : ""}
-        ]{
-        _updatedAt,
-        _id,
-        title,
-        "url": ^.sitemap[_id == ^._id][0] {
-          "path": 'https://' + ^.^.domain + '' + path
-        }.path,
-        "body": array::join(blocks[
-            !(_type in path('studio.*')) 
-            && disabled != true
-          ] {
-          "text": array::join(array::compact([
-            ^.description,
-            ^.seo.title,
-            ^.seo.description,
-            array::join(array::compact(^.tags[]->title), ' '),
-            title,
-            pt::text(title), 
-            pt::text(intro), 
-            pt::text(body), 
-            pt::text(content), 
-            pt::text(footer),
-
-            // faq
-            ...faq[] {
-              ...coalesce(@ ->, @) {
-                "text": array::join([
-                  title,
-                  pt::text(content),
-                ], ' '),
-              }
-            }.text,
-
-            // cards
-            ...items[] {
-              _type == "card.composable" => {
-                "text": array::join(array::compact([
-                  title,
-                  subtitle,
-                  pt::text(content),
-                ]), ' ')
-              },
-
-              // testimonial reference
-              _type == "card.testimonial" && defined(testimonialRef) => @.testimonialRef-> {
-                "text": array::join(array::compact([
-                  title,
-                  subtitle,
-                  pt::text(content),
-                ]), ' ')
-              },
-
-              // plain testimonial
-              _type == "card.testimonial" && defined(testimonial) => testimonial{
-                "text": array::join(array::compact([
-                  title,
-                  subtitle,
-                  pt::text(content),
-                ]), ' ')
-              }
-              
-            }.text
-          ]), ' '),
-        }.text, '')
-      }
-    }.page[]`,
+    getExportQuery({ id: id as string, type: type as string }),
   );
 
   if (!allPages) {
