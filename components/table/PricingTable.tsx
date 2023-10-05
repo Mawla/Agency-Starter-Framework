8650b2b53616cb35a618005a1470611abba93e44
Arjen <aratramba@gmail.com>
2023-10-05 07:41:19 +0200

    Update PricingTable.tsx

--- components/table/PricingTable.tsx
+++ components/table/PricingTable.tsx
@@ -23,28 +23,44 @@ const textTransformers = [
     // regex for only yes or YES character in the string
     regex: /(^|\s)([yY][eE][sS])(\s|$)/gim,
     fn: (key: number, result: RegExpExecArray) => (
-      <IconLoader icon="yes" className="inline-block align-middle w-5 h-5" />
+      <IconLoader
+        icon="yes"
+        className="inline-block align-middle w-5 h-5"
+        removeColors={false}
+      />
     ),
   },
   {
     // regex for only v or V or y or Y character in the string
     regex: /(^|\s)([vV]|[yY])(\s|$)/gim,
     fn: (key: number, result: RegExpExecArray) => (
-      <IconLoader icon="yes" className="inline-block align-middle w-5 h-5" />
+      <IconLoader
+        icon="yes"
+        className="inline-block align-middle w-5 h-5"
+        removeColors={false}
+      />
     ),
   },
   {
     // regex for only x or X or n or N character in the string
     regex: /(^|\s)([xX]|[nN])(\s|$)/gim,
     fn: (key: number, result: RegExpExecArray) => (
-      <IconLoader icon="no" className="inline-block align-middle w-5 h-5" />
+      <IconLoader
+        icon="no"
+        className="inline-block align-middle w-5 h-5"
+        removeColors={false}
+      />
     ),
   },
   {
     // regex for only no or NO character in the string
     regex: /(^|\s)([nN][oO])(\s|$)/gim,
     fn: (key: number, result: RegExpExecArray) => (
-      <IconLoader icon="no" className="inline-block align-middle w-5 h-5" />
+      <IconLoader
+        icon="no"
+        className="inline-block align-middle w-5 h-5"
+        removeColors={false}
+      />
     ),
   },
 ];
