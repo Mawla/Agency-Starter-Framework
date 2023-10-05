74e50f5a68a8baddf11387335d7e0fce91619421
Arjen <aratramba@gmail.com>
2023-10-05 07:31:47 +0200

    tweak mobile alignment pricing card

--- blocks/block3/block3.classes.ts
+++ blocks/block3/block3.classes.ts
@@ -4,6 +4,6 @@ export const gridClasses: Record<ColumnType, string> = {
   0: "sm:grid-cols-2 lg:grid-cols-4",
   1: "grid-cols-3",
   2: "sm:grid-cols-2",
-  3: "sm:grid-cols-2 lg:grid-cols-3",
+  3: "sm:grid-cols-2 sm:[&>div:nth-of-type(3)]:col-span-2 lg:grid-cols-3 lg:[&>div:nth-of-type(3)]:col-span-1",
   4: "sm:grid-cols-2 lg:grid-cols-3",
 };
