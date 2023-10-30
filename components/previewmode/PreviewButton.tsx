8c605246aa0200671fc8f9ef34e1a051e70e923d
Arjen <aratramba@gmail.com>
2023-10-30 14:56:29 +0100

    Update PreviewButton.tsx

--- components/previewmode/PreviewButton.tsx
+++ components/previewmode/PreviewButton.tsx
@@ -22,9 +22,9 @@ export const PreviewButton = ({ pagePath }: { pagePath: string }) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="w-5 h-5"
         >
           <path d="M18 6 6 18" />
