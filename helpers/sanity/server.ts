e4e4699a49d444f347a91ccd827a96069fd8e7c8
Arjen <aratramba@gmail.com>
2023-11-01 14:09:13 +0100

    Update server.ts

--- helpers/sanity/server.ts
+++ helpers/sanity/server.ts
@@ -1,18 +1,10 @@
 import { config } from "./config";
-import { createClient } from "@sanity/client";
-
-/**
- * Set up a client without preview authorization
- */
-
-const mockClient = {
-  fetch: async (query: string) => null,
-};
-
-/**
- * Helper function for easily switching between normal client and preview client
- */
+import { createClient } from "next-sanity";
 
 export const getClient = () => {
-  return config.projectId && config.dataset ? createClient(config) : mockClient;
+  return config.projectId && config.dataset
+    ? createClient(config)
+    : {
+        fetch: async (query: string) => null,
+      };
 };
