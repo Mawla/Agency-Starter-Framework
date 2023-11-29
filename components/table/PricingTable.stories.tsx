76c914c7fe529fae049c98faa1532a0a4fffb4cb
Arjen <aratramba@gmail.com>
2023-11-29 09:55:36 +0100

    update pricing table stories

--- components/table/PricingTable.stories.tsx
+++ components/table/PricingTable.stories.tsx
@@ -7,13 +7,64 @@ export default {
   title: "Components/PricingTable",
 } as Meta;
 
-export const Default = () => (
+export const File = () => (
   <PricingTable
     features={[
       {
         _id: "1",
         title: "title",
         csv: "/storybook/pricing.csv",
+      },
+    ]}
+  />
+);
+
+export const Text = () => (
+  <PricingTable
+    features={[
+      {
+        _id: "1",
+        title: "title",
+        csv: "Usage;Starter;Growth;Business;Enterprise\nEditors and admins;1;2;Custom;Contact us",
+      },
+    ]}
+  />
+);
+
+export const TextTransforms = () => (
+  <PricingTable
+    features={[
+      {
+        _id: "1",
+        title: "title",
+        csv: ",,,,\ny,Y,yes,Yes,\nn,N,no,nO,\nx,X,,,",
+      },
+    ]}
+  />
+);
+
+export const Tooltips = () => (
+  <PricingTable
+    features={[
+      {
+        _id: "1",
+        title: "title",
+        csv: `,
+(i=tooltip),
+(i=tooltip (text)),
+(i=tooltip (more) (text)),
+(i=(tooltip text)),
+(i= tooltip),
+(i= tooltip (text)),
+(i= tooltip (more) (text)),
+(i= (tooltip text)),
+,
+not supported,
+(i=(brackets (within brackets))),
+(i= (brackets (within brackets))),
+(a=tooltip),
+(i tooltip),
+`,
       },
     ]}
   />
